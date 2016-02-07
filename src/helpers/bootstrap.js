b12192e8ca0e3a104a2cdf7c5f015ae5d541adf2
Antoine Jackson <a.j.william26@gmail.com>
2016-02-07 16:28:28 +0100

    fix(bootstrap): fix error in bootstrap with no run
    `jedi.bootstrap` threw an error when no runnable was
    given to the bootstrapped module.

--- src/helpers/bootstrap.js
+++ src/helpers/bootstrap.js
@@ -5,8 +5,9 @@ const resolve = require('./resolve');
 
 function bootstrap(module) {
   module.dependencies.map(bootstrap);
-
-  module.runnableMethod.apply(module, module.resolve(argsList(module.runnableMethod)));
+  if (module.runnableMethod) {
+    module.runnableMethod.apply(module, module.resolve(argsList(module.runnableMethod)));
+  }
 };
 
 module.exports = bootstrap;
