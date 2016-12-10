553e6cc602bb98b4eb9bad366574d1b159213e82
Antoine Jackson <a.j.william26@gmail.com>
2016-12-10 23:49:12 +0100

    feat(resolver): add  management (#40)

--- src/helpers/args-list.js
+++ src/helpers/args-list.js
@@ -4,6 +4,9 @@ export function getArgsList(target) {
   if (typeof target === 'object' && target.length && typeof target[target.length - 1] === 'function') {
     return target.slice(0, target.length - 1);
   }
+  if (typeof target === 'function' && !!target.$inject) {
+    return target.$inject;
+  }
   if (typeof target === 'function') {
     return argsList(target);
   }
